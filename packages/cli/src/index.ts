912dcc0c771c4eeebef3c4d9bd6f35a50d798899
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2024-04-09 20:25:01 +0100

    refactor: make error handling logic cleaner and doesn't affect original API

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,7 +2,7 @@
 import { run, command, positional, string, optional, option } from "cmd-ts";
 import updater from "tiny-updater";
 import { name, version } from "../package.json";
-import { downloadAndExtract } from "@begit/core";
+import { downloadRepo } from "@begit/core";
 const main = async () => {
 	updater({ name, version, ttl: 86_400_000 });
 	const cli = command({
@@ -26,7 +26,7 @@ const main = async () => {
 			const owner = parts.pop();
 			const branch = url.split("#")[1] as string | undefined;
 			if (!repo || !owner) throw new Error("Invalid URL");
-			await downloadAndExtract({
+			await downloadRepo({
 				repo: { owner, name: repo, branch, subdir },
 				dest,
 			});
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -79,21 +79,22 @@ export const downloadAndExtract = async ({
 	const caching = opts.cache;
 	cwd = cwd ?? process.cwd();
 	dest = dest ?? repo.name;
-	let tarPath = await downloadToFile(repo);
-	try {
-		await extractFile(tarPath, join(cwd, dest), repo.subdir);
-	} catch (e: any) {
-		const t = e.tarCode;
-		if (t === "TAR_ABORT") {
-			// Unable to extract tarfile, assume cache is corrupted and refetch
-			await unlink(tarPath);
-			tarPath = await downloadToFile(repo);
-			await extractFile(tarPath, join(cwd, dest), repo.subdir);
-		}
-	}
+	const tarPath = await downloadToFile(repo);
+	await extractFile(tarPath, join(cwd, dest), repo.subdir);
 
 	// Remove tarball download if not caching
 	if (!caching) {
 		await unlink(tarPath);
+	}
+};
+export const downloadRepo = async (opts: DownloadAndExtract) => {
+	try {
+		await downloadAndExtract(opts);
+	} catch (e: any) {
+		if (e.tarCode !== "TAR_ABORT") throw e;
+		// Assume tarball is corrupted and attempt to refetch
+		const tarPath = e.file as string;
+		await unlink(tarPath);
+		await downloadAndExtract(opts);
 	}
 };
