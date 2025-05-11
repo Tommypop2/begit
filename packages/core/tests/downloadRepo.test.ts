7a2582aaa7cd57ccb4bd02b658db21232bd0c99b
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2025-05-11 19:24:40 +0100

    test: fully test `downloadRepo`

---
+++ packages/core/tests/downloadRepo.test.ts
@@ -0,0 +1,16 @@
+import { mkdir, rm } from "fs/promises";
+import { beforeAll, describe, expect, it, test } from "vitest";
+import { downloadRepo } from "../src";
+import { existsSync } from "fs";
+
+describe("downloadRepo", () => {
+	beforeAll(async () => {
+		if (existsSync("./tmp_test"))
+			await rm("./tmp_test", { recursive: true })
+	})
+
+	it("downloads repo to folder", async () => {
+		await downloadRepo({ repo: { owner: "Tommypop2", name: "begit" }, dest: "./tmp_test/begit" })
+		expect(existsSync("./tmp_test/begit/package.json") && existsSync("./tmp_test/begit/packages/core/src/index.ts"))
+	})
+})
